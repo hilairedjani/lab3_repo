23df5d348a05e6d8ca5ff1e029419ac56860a762
hilairedjani <djanilewis1@gmail.com>
2019-09-19 09:15:04 -0400

    Initial commit

---
+++ src/AddressBook.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+
+/**
+ * @author Hilaire Lewis
+ *
+ */
+public class AddressBook {
+	
+	private BuddyInfo [] buddyInfos;
+
+	/**
+	 * 
+	 */
+	public AddressBook() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * This method adds a new buddy to the buddy info array
+	 */
+	public boolean addBuddy() {
+		return true;
+	}
+	
+	/**
+	 * This method removes a new buddy to the buddy info array
+	 */
+	public boolean removeBuddy() {
+		return true;
+	}
+	
+	/**
+	 * Main method for AddressBook class
+	 */
+	public static void main(String [] args) {
+		System.out.println("Addess Book");
+	}
+
+}
